4f35db9bc6c0a8d647c7b9d643be23466ce82473
simonmaris <simon@simonmaris.com>
2021-05-17 11:32:43 +0200

    workflow: new method initial test

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+DEST="${JEKYLL_DESTINATION:-_site}"
+REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+BRANCH="gh-pages"
+BUNDLE_BUILD__SASSC=--disable-march-tune-native
+
+echo "Installing gems..."
+
+bundle config path vendor/bundle
+bundle install --jobs 4 --retry 3
+
+echo "Building Jekyll site..."
+
+JEKYLL_ENV=production NODE_ENV=production bundle exec jekyll build
+
+echo "Publishing..."
+
+cd ${DEST}
+
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git add .
+git commit -m "published by GitHub Actions"
+git push --force ${REPO} master:${BRANCH}
